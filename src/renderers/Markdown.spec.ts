25a6e6a82ec00123cfe7b9428d3985d6e0daaf0e
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-01-16 19:55:11 -0500

    tdd: Markdown is the first Renderer implementation

---
+++ src/renderers/Markdown.spec.ts
@@ -0,0 +1,16 @@
+import assert from "assert";
+import { Clipboard } from "../Clipboard";
+import { Link } from "../Link";
+import { Markdown } from "./Markdown";
+
+test('should render a simple link', () => {
+    let cut : Markdown = new Markdown();
+    let link : Link = {
+        text: "example",
+        destination: "https://www.example.com",
+    };
+
+    let actual : Clipboard = cut.render(link);
+
+    assert.equal("[example](https://www.example.com)", actual.data);
+})
---
+++ src/renderers/Markdown.ts
@@ -0,0 +1,13 @@
+import { Clipboard } from "../Clipboard";
+import { Link } from "../Link";
+import { Renderer } from "../Renderer";
+
+export class Markdown implements Renderer {
+    render(link: Link): Clipboard {
+        let result : Clipboard = {
+            data: `[${link.text}](${link.destination})`,
+            typeInfo: "text"
+        };
+        return result;
+    }
+}
