f488d9cd67bc52e8480a7d9b32434ac5496f56c6
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-01-16 21:21:16 -0500

    tdd: Textile is the 3rd Renderer implementation

---
+++ src/renderers/Textile.spec.ts
@@ -0,0 +1,16 @@
+import assert from "assert";
+import { Clipboard } from "../Clipboard";
+import { Link } from "../Link";
+import { Textile } from "./Textile";
+
+test('should render a simple link', () => {
+    let cut : Textile = new Textile();
+    let link : Link = {
+        text: "example",
+        destination: "https://www.example.com",
+    };
+
+    let actual : Clipboard = cut.render(link);
+
+    assert.equal('[example|https://www.example.com]', actual.data);
+})
