e10eed02791ed9cc6209830598bc6d8ce23db950
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-01-20 18:59:47 -0500

    refactor: simplify & remove duplication

--- src/parsers/Default.ts
+++ src/parsers/Default.ts
@@ -4,19 +4,10 @@ import { Parser } from "../Parser";
 export class Default implements Parser {
     parseLink(doc: Document, url: string): Link | null {
         const titleElement : HTMLElement | null = doc.querySelector("html head title");
-        if (titleElement) {
-            const result : Link = {
-                text: titleElement.innerHTML,
-                destination: url,
-            };
-            return result;
-        }
-        else {
-            const result : Link = {
-                text: url,
-                destination: url,
-            };
-            return result;
-        }
+        const result : Link = {
+            text: titleElement?.innerHTML ?? url,
+            destination: url,
+        };
+        return result;
     }
 }
