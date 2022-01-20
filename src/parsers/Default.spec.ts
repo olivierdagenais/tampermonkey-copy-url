078d2d9f0f64c98672dbed82a016034c46eea89f
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-01-20 18:58:07 -0500

    test: page without a title

--- src/parsers/Default.spec.ts
+++ src/parsers/Default.spec.ts
@@ -30,3 +30,29 @@ test('should parse a simple HTML page', () => {
     assert.equal(actual?.destination, "https://example.com");
     assert.equal(actual?.text, "Example Domain");
 })
+
+test('should parse a page without a title', () => {
+    const html = `
+    <html><head>
+</head>
+
+<body>
+<div>
+    <h1>Example Domain</h1>
+    <p>This domain is for use in illustrative examples in documents. You may use this
+    domain in literature without prior coordination or asking for permission.</p>
+    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
+</div>
+
+
+</body></html>`;
+    const dom : JSDOM = new JSDOM(html);
+    const document : Document = dom.window.document;
+    const cut : Parser = new Default();
+
+    const actual : Link | null = cut.parseLink(document, "https://example.com");
+
+    assert.notEqual(actual, null);
+    assert.equal(actual?.destination, "https://example.com");
+    assert.equal(actual?.text, "https://example.com");
+})
