fdf64bf47ea6d9fb0f18e3f9d3b9b24e57af15cc
Olivier Dagenais <odagenais@kinaxis.com>
2022-10-28 11:13:32 -0400

    tdd: simple Bitbucket pull request page

--- src/parsers/Bitbucket.spec.ts
+++ src/parsers/Bitbucket.spec.ts
@@ -13,5 +13,26 @@ function testParseLink(html: string, url: string): Link | null {
     return actual;
 }
 
-test("TODO", () => {
+test("should parse a simple Bitbucket pull request page", () => {
+    const html = `
+<html>
+    <head>
+        <title>Pull Request #4: DO-8731: fix: sanitize GitHub PR comment bodies - Stash</title>
+    </head>
+</html>`;
+
+    const actual = testParseLink(
+        html,
+        "https://bitbucket.example.com/projects/TP/repos/jf_agent/pull-requests/4/overview"
+    );
+
+    assert.notEqual(actual, null);
+    assert.equal(
+        actual?.destination,
+        "https://bitbucket.example.com/projects/TP/repos/jf_agent/pull-requests/4/overview"
+    );
+    assert.equal(
+        actual?.text,
+        "TP/jf_agent#4: DO-8731: fix: sanitize GitHub PR comment bodies"
+    );
 });
--- src/parsers/Bitbucket.ts
+++ src/parsers/Bitbucket.ts
@@ -3,6 +3,37 @@ import { Parser } from "../Parser";
 
 export class Bitbucket implements Parser {
     parseLink(doc: Document, url: string): Link | null {
-        return null;
+        const prUrlRegex =
+            /https:\/\/(?<host>[^/]+)\/projects\/(?<project>[^/]+)\/repos\/(?<repo>[^/]+)\/pull-requests\/(?<prId>\d+).*/;
+        const prUrlMatch = url.match(prUrlRegex);
+        if (!prUrlMatch || !prUrlMatch.groups) {
+            return null;
+        }
+        const prUrlGroups = prUrlMatch.groups;
+        const project = prUrlGroups.project;
+        const repo = prUrlGroups.repo;
+        const prId = prUrlGroups.prId;
+
+        const titleElement: HTMLElement | null =
+            doc.querySelector("html head title");
+        if (!titleElement) {
+            return null;
+        }
+        const titleString = titleElement.textContent;
+        const prTitleRegex =
+            /Pull Request #(?<prId>\d+): (?<summary>.+) - Stash/;
+        const prTitleMatch = titleString?.match(prTitleRegex);
+        if (!prTitleMatch || !prTitleMatch.groups) {
+            return null;
+        }
+        const prTitleGroups = prTitleMatch.groups;
+        const summary = prTitleGroups.summary;
+
+        const linkText = `${project}/${repo}#${prId}: ${summary}`;
+        const result: Link = {
+            text: linkText,
+            destination: url,
+        };
+        return result;
     }
 }
