ab452d7788abb6d7fbf6b099fa1142176c211810
Olivier Dagenais <olivier.dagenais@gmail.com>
2023-01-12 14:01:29 -0500

    release 1.3.1

--- userscript/index.user.js
+++ userscript/index.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name tampermonkey-copy-url
-// @version 1.3.0
+// @version 1.3.1
 // @namespace https://olivierdagenais.github.io/
 // @description A Tampermonkey userscript to copy nice-looking URLs to the clipboard.
 // @author Olivier Dagenais
