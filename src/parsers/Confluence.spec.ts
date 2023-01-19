b141c7b14d9725bd5582ad4a76f89eb1790f2a86
Olivier Dagenais <olivier.dagenais@gmail.com>
2023-01-19 09:08:55 -0500

    feat: implement Confluence parser, w/test

--- src/parsers/Confluence.spec.ts
+++ src/parsers/Confluence.spec.ts
@@ -14,5 +14,40 @@ function testParseLink(html: string, url: string): Link | null {
 }
 
 test("should parse a simple page", () => {
-    // TODO: implement test
+    const html = `
+<html>
+    <head>
+        <title>Supported AWS Objects - Cloudaware Documentation (Public) - Confluence</title>
+        <meta name="ajs-page-title" content="Supported AWS Objects">
+    </head>
+    <body
+        id="com-atlassian-confluence"
+        class="theme-default dashboard aui-layout aui-theme-default"
+    >
+        <div class="PageContent">
+            <!-- etc., etc. -->
+            <h1
+                id="title-text"
+                data-test-id="title-text"
+                data-testid="title-text"
+                style="color: rgb(23, 43, 77);"
+                class="css-1agkp1r e1vqopgf1"
+            >
+                Supported AWS Objects
+            </h1>
+        </div>
+    </body>
+</html>`;
+
+    const actual = testParseLink(
+        html,
+        "https://cloudaware.atlassian.net/wiki/spaces/DOCS/pages/3190554625/Supported+AWS+Objects"
+    );
+
+    assert.notEqual(actual, null);
+    assert.equal(
+        actual?.destination,
+        "https://cloudaware.atlassian.net/wiki/spaces/DOCS/pages/3190554625/Supported+AWS+Objects"
+    );
+    assert.equal(actual?.text, "Supported AWS Objects");
 });
--- src/parsers/Confluence.ts
+++ src/parsers/Confluence.ts
@@ -3,7 +3,19 @@ import { Link } from "../Link";
 
 export class Confluence extends AbstractParser {
     parseLink(doc: Document, url: string): Link | null {
-        var linkText = `TODO`;
+        const bodyElement: HTMLElement | null = doc.querySelector(
+            "#com-atlassian-confluence"
+        );
+        if (!bodyElement) {
+            return null;
+        }
+
+        const titleElement: HTMLElement | null =
+            doc.querySelector("#title-text");
+        if (!titleElement || !titleElement.textContent) {
+            return null;
+        }
+        var linkText = titleElement.textContent.trim();
         const result: Link = {
             text: linkText,
             destination: url,
