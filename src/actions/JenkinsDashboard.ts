ebc5ad6622b8141fa5d8025f18cfb5cfdbe75dfe
Olivier Dagenais <olivier.dagenais@gmail.com>
2023-04-14 21:28:07 -0400

    feat: go to dashboard from job page

--- src/actions/JenkinsDashboard.ts
+++ src/actions/JenkinsDashboard.ts
@@ -1,7 +1,38 @@
 import { GoToAction } from "./GoToAction";
 
 export class JenkinsDashboard extends GoToAction {
-    navigate(doc: Document, url: string): string | null {
+    buildUrl(urlString: string, newPath: string) : string {
+        const url = new URL(urlString);
+        url.pathname = newPath;
+        return url.toString();
+    }
+
+    navigate(doc: Document, urlString: string): string | null {
+        const bodyElement: HTMLElement | null = doc.querySelector(
+            "html body[id=jenkins]"
+        );
+        if (!bodyElement) {
+            return null;
+        }
+
+        var selector = ".jenkins-breadcrumbs__list-item";
+        const jenkinsVersion = bodyElement.getAttribute("data-version");
+        switch (jenkinsVersion) {
+            case "2.361.4":
+                selector = ".jenkins-breadcrumbs .item";
+                break;
+        }
+
+        const breaCrumbListItem = doc.querySelector(selector);
+        if (breaCrumbListItem) {
+            const anchor = breaCrumbListItem.querySelector("a");
+            if (anchor) {
+                const path = anchor.getAttribute("href");
+                if (path) {
+                    return this.buildUrl(urlString, path);
+                }
+            }
+        }
         return null;
     }
 }
