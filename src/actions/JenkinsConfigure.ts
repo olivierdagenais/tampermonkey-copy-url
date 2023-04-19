9ef9ee136de13688cb56837a0261fa984f9a4155
Olivier Dagenais <olivier.dagenais@gmail.com>
2023-04-18 21:41:49 -0400

    tdd: JenkinsConfigure from run page

--- src/actions/JenkinsConfigure.ts
+++ src/actions/JenkinsConfigure.ts
@@ -9,9 +9,28 @@ export class JenkinsConfigure extends GoToAction {
         }
 
         const selector = JenkinsHelpers.getBreadcrumbItemSelector(bodyElement);
-
         const breaCrumbListItems = doc.querySelectorAll(selector);
         if (breaCrumbListItems) {
+            const urlParts = JenkinsHelpers.splitUrlPath(urlString);
+            const isUrlToRun =
+                "job" === urlParts[urlParts.length - 3] &&
+                JenkinsHelpers.isInteger(urlParts[urlParts.length - 1]);
+            if (isUrlToRun) {
+                const penultimateItem = breaCrumbListItems.item(
+                    breaCrumbListItems.length - 2
+                );
+                const anchor = penultimateItem.querySelector("a");
+                if (anchor) {
+                    const path = anchor.getAttribute("href");
+                    if (path) {
+                        return (
+                            JenkinsHelpers.buildUrl(urlString, path) +
+                            "configure"
+                        );
+                    }
+                }
+                return null;
+            }
             const lastItem = breaCrumbListItems.item(
                 breaCrumbListItems.length - 1
             );
