5eb6b968aadd5037f97791c8d80d285e98e53ed6
Olivier Dagenais <olivier.dagenais@gmail.com>
2023-05-17 21:19:31 -0400

    refactor: use regex only for JIRA URL path

--- src/parsers/Jira.ts
+++ src/parsers/Jira.ts
@@ -1,19 +1,23 @@
 import { AbstractParser } from "./AbstractParser";
 import { Link } from "../Link";
-
-const issueUrlRegex =
-    /https:\/\/(?<host>[^/]+)\/([^/]+\/)*browse\/(?<issueKey>[^?]+)(\?(?<rest>.+))?/;
+const issuePathRegex =
+    /browse\/(?<issueKey>[A-Z][A-Z0-9]+-\d+)/;
 export class Jira extends AbstractParser {
     parseLink(doc: Document, url: string): Link | null {
-        const issueUrlMatch = url.match(issueUrlRegex);
-        if (!issueUrlMatch || !issueUrlMatch.groups) {
+        const u = new URL(url);
+        const path = u.pathname;
+        if (path.indexOf("browse/") == -1) {
             return null;
         }
-        const issueUrlGroups = issueUrlMatch.groups;
-        const issueKey = issueUrlGroups.issueKey;
+        const pathMatch = path.match(issuePathRegex);
+        if (!pathMatch || !pathMatch.groups) {
+            return null;
+        }
+        const pathGroups = pathMatch.groups;
+        const issueKey = pathGroups.issueKey;
         var linkText = `${issueKey}`;
 
-        const h1Element : HTMLElement | null = doc.querySelector("#summary-val");
+        const h1Element: HTMLElement | null = doc.querySelector("#summary-val");
         if (h1Element) {
             const summary = h1Element.textContent;
             if (summary) {
