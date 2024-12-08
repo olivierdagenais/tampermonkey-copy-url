61d207e6b72fa17860c1b049f08989e86506db56
Olivier Dagenais <olivier.dagenais@gmail.com>
2024-12-08 16:15:38 -0500

    skeleton: SemVer class & test

---
+++ src/SemVer.ts
@@ -0,0 +1,12 @@
+
+export class SemVer {
+    readonly major: number;
+    readonly minor: number;
+    readonly patch: number;
+
+    constructor(major: number, minor: number, patch: number) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+    }
+}
