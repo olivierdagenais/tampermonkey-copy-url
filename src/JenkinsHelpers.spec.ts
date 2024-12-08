30b2237dbf1575791f7445ee6ab2a75a10caaaf0
Olivier Dagenais <olivier.dagenais@gmail.com>
2024-12-08 17:23:59 -0500

    helper: getMostRecentRunSelector, w/tests

--- src/JenkinsHelpers.spec.ts
+++ src/JenkinsHelpers.spec.ts
@@ -1,5 +1,88 @@
 import { assert, test } from "vitest";
+import { JSDOM } from "jsdom";
 import { JenkinsHelpers } from "./JenkinsHelpers";
+
+function getBodyElement(html: string): HTMLElement {
+    const dom: JSDOM = new JSDOM(html);
+    const document: Document = dom.window.document;
+    const result = JenkinsHelpers.getBodyElement(document);
+    if (!result) {
+        throw new Error("Invalid HTML");
+    }
+    return result;
+}
+
+test("getMostRecentRunSelector at 2.387.1", () => {
+    const html = `
+<html>
+    <head>
+        <title>Branch [Project » Repository] [Jenkins]</title>
+    </head>
+    <body
+        data-model-type="org.jenkinsci.plugins.workflow.job.WorkflowRun"
+        id="jenkins"
+        class="yui-skin-sam two-column jenkins-2.387.1"
+        data-version="2.387.1"
+        >
+    </body>
+</html>
+`;
+    const bodyElement = getBodyElement(html);
+
+    const actual = JenkinsHelpers.getMostRecentRunSelector(bodyElement);
+
+    assert.equal(actual, "tr.build-row a.build-link");
+});
+
+test("getMostRecentRunSelector at 2.463", () => {
+    const html = `
+<html>
+    <head>
+        <title>Branch [Project » Repository] [Jenkins]</title>
+    </head>
+    <body
+        data-model-type="org.jenkinsci.plugins.workflow.job.WorkflowRun"
+        id="jenkins"
+        class="yui-skin-sam two-column jenkins-2.463"
+        data-version="2.463"
+        >
+    </body>
+</html>
+`;
+    const bodyElement = getBodyElement(html);
+
+    const actual = JenkinsHelpers.getMostRecentRunSelector(bodyElement);
+
+    assert.equal(
+        actual,
+        "#jenkins-build-history .app-builds-container__item__inner__link"
+    );
+});
+
+test("getMostRecentRunSelector at 2.479.2", () => {
+    const html = `
+<html>
+    <head>
+        <title>Branch [Project » Repository] [Jenkins]</title>
+    </head>
+    <body
+        data-model-type="org.jenkinsci.plugins.workflow.job.WorkflowRun"
+        id="jenkins"
+        class="yui-skin-sam two-column jenkins-2.479.2"
+        data-version="2.479.2"
+        >
+    </body>
+</html>
+`;
+    const bodyElement = getBodyElement(html);
+
+    const actual = JenkinsHelpers.getMostRecentRunSelector(bodyElement);
+
+    assert.equal(
+        actual,
+        "#jenkins-build-history .app-builds-container__item__inner__link"
+    );
+});
 
 test("splitUrlPath with deep URL", () => {
     const input =
--- src/JenkinsHelpers.ts
+++ src/JenkinsHelpers.ts
@@ -1,3 +1,7 @@
+import { SemVer } from "./SemVer";
+
+const jenkins2_463 = SemVer.parse("2.463");
+
 export class JenkinsHelpers {
     static buildUrl(
         urlString: string,
@@ -28,6 +32,20 @@ export class JenkinsHelpers {
                 break;
         }
         return selector;
+    }
+
+    static getMostRecentRunSelector(bodyElement: HTMLElement): string {
+        const jenkinsVersion = bodyElement.getAttribute("data-version");
+        if (!jenkinsVersion) {
+            throw new Error("I can't determine the Jenkins version!");
+        }
+        const jenkinsSemVer = SemVer.parse(jenkinsVersion);
+        // jenkinsci/jenkins#9148: Rewrite the build history widget by janfaracik
+        // https://github.com/jenkinsci/jenkins/pull/9148
+        if (jenkins2_463.compareTo(jenkinsSemVer) <= 0) {
+            return "#jenkins-build-history .app-builds-container__item__inner__link";
+        }
+        return "tr.build-row a.build-link";
     }
 
     static isInteger(s: string): boolean {
