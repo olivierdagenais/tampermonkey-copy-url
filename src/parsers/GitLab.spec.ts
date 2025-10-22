6f9dafc7c3d3c447de7a67debbdfa8b52c1d272e
Olivier Dagenais <oli@bravodelta.ca>
2025-10-22 12:06:49 -0400

    feat: link to project

--- src/parsers/GitLab.spec.ts
+++ src/parsers/GitLab.spec.ts
@@ -13,7 +13,40 @@ function testParseLink(html: string, url: string): Link | null {
     return actual;
 }
 
-// TODO: add tests
 test("link to project", () => {
+    const html: string = `
+<html>
+<head>
+    <meta content="GitLab" property="og:site_name">
+    <meta content="A multiplatform Matrix messenger SDK using Trixnity." property="og:description">
+</head>
+<body
+    data-group="trixnity-messenger"
+    data-group-full-path="connect2x/trixnity-messenger"
+    data-namespace-id="76644306"
+    data-page="projects:show"
+    data-page-type-id="trixnity-messenger"
+    data-project="trixnity-messenger"
+    data-project-full-path="connect2x/trixnity-messenger/trixnity-messenger"
+    data-project-id="47538655"
+    data-project-studio-available="false"
+    data-project-studio-enabled="false"
+    >
+</body>
+</html>
+`;
+    const actual = testParseLink(
+        html,
+        "https://gitlab.com/connect2x/trixnity-messenger/trixnity-messenger"
+    );
 
+    assert.notEqual(actual, null);
+    assert.equal(
+        actual?.destination,
+        "https://gitlab.com/connect2x/trixnity-messenger/trixnity-messenger"
+    );
+    assert.equal(
+        actual?.text,
+        "connect2x/trixnity-messenger/trixnity-messenger: A multiplatform Matrix messenger SDK using Trixnity."
+    );
 });
--- src/parsers/GitLab.ts
+++ src/parsers/GitLab.ts
@@ -5,8 +5,29 @@ import {GitLabData} from "./GitLabData";
 export class GitLab extends AbstractParser {
 
     parseLink(doc: Document, url: string): Link | null {
-        // TODO: implement issues, pull requests, wiki pages, etc.
-        return null;
+        const siteNameSelector =
+            "html head meta[property='og:site_name'][content='GitLab']";
+        const siteName: HTMLElement | null =
+            doc.querySelector(siteNameSelector);
+        if (!siteName) {
+            return null;
+        }
+        const data = new GitLabData(doc);
+        let linkText: string;
+        switch (data.page) {
+            case "projects:show":
+                linkText = `${data.projectFullPath}: ${data.description}`;
+                break;
+            // TODO: implement issues, pull requests, wiki pages, etc.
+            default:
+                linkText = data.page;
+                break;
+        }
+        const result: Link = {
+            text: linkText,
+            destination: url,
+        };
+        return result;
     }
 
 }
