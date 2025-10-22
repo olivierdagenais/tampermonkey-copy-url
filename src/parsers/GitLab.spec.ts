387ae0c8f3b2cdb1763f95f563d5e048f549a2e9
Olivier Dagenais <oli@bravodelta.ca>
2025-10-22 13:49:02 -0400

    feat: link to issue, has page-type-id

--- src/parsers/GitLab.spec.ts
+++ src/parsers/GitLab.spec.ts
@@ -50,3 +50,64 @@ test("link to project", () => {
         "connect2x/trixnity-messenger/trixnity-messenger: A multiplatform Matrix messenger SDK using Trixnity."
     );
 });
+
+test("link to issue, has page-type-id", () => {
+    const html: string = `
+<html>
+<head>
+    <meta content="GitLab" property="og:site_name">
+    <meta content="A multiplatform Matrix messenger SDK using Trixnity." property="og:description">
+    <meta content="Room name &amp; topic are not accessible (#686) · Issues · connect2x / Trixnity Messenger / Trixnity Messenger · GitLab" property="og:title">
+</head>
+<body
+    data-group="trixnity-messenger"
+    data-group-full-path="connect2x/trixnity-messenger"
+    data-namespace-id="76644306"
+    data-page="projects:issues:show"
+    data-page-type-id="686"
+    data-project="trixnity-messenger"
+    data-project-full-path="connect2x/trixnity-messenger/trixnity-messenger"
+    data-project-id="47538655"
+    data-project-studio-available="false"
+    data-project-studio-enabled="false"
+    >
+    <li class="gl-breadcrumb-item gl-breadcrumb-item-sm">
+        <a
+            href="/connect2x/trixnity-messenger/trixnity-messenger/-/issues/686"
+            aria-current="page"
+            class="router-link-exact-active router-link-active"
+            >
+            <!---->
+            <span class="gl-align-middle">#686</span>
+        </a>
+    </li>
+    <div
+        data-testid="work-item-type"
+        class="@sm/panel:!gl-block gl-w-full"
+        >
+        <h1
+            data-testid="work-item-title"
+            class="gl-heading-1 !gl-m-0 gl-w-full gl-wrap-anywhere"
+            >
+            <span>Room name &amp; topic are not accessible</span>
+        </h1>
+    </div>
+</body>
+</html>
+`;
+    const actual = testParseLink(
+        html,
+        "https://gitlab.com/connect2x/trixnity-messenger/trixnity-messenger/-/issues/686"
+    );
+
+    assert.notEqual(actual, null);
+    assert.equal(
+        actual?.destination,
+        "https://gitlab.com/connect2x/trixnity-messenger/trixnity-messenger/-/issues/686"
+    );
+    assert.equal(
+        actual?.text,
+        "connect2x/trixnity-messenger/trixnity-messenger#686: " +
+            "Room name & topic are not accessible"
+    );
+});
