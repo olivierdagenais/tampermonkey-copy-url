e5f80fafba71c4c599953f4a56d6018b29783983
Olivier Dagenais <oli@bravodelta.ca>
2026-02-04 22:18:06 -0500

    refactor: centralize, re-use code in JenkinsPage

--- src/JenkinsPage.ts
+++ src/JenkinsPage.ts
@@ -15,4 +15,34 @@ export class JenkinsPage {
         this.crumbs = crumbs;
     }
 
+    buildUrl(path: string, queryString?: string): string {
+        return JenkinsHelpers.buildUrl(
+            this.urlString,
+            path,
+            queryString
+        );
+    }
+
+    eachCrumb(callback: (value: Crumb, key?: number) => boolean): void {
+        for (let key = 0; key < this.crumbs.length - 1; key++) {
+            const keepGoing = callback(this.crumbs[key], key);
+            if (!keepGoing) {
+                break;
+            }
+        }
+    }
+
+    lastCrumb(): Crumb {
+        return this.crumbs[this.crumbs.length - 1];
+    }
+
+    reverseCrumb(callback: (value: Crumb, key?: number) => boolean): void {
+        for (let key = this.crumbs.length - 1; key > 0; key--) {
+            const keepGoing = callback(this.crumbs[key], key);
+            if (!keepGoing) {
+                break;
+            }
+        }
+    }
+
 }
--- src/actions/JenkinsPrevious.ts
+++ src/actions/JenkinsPrevious.ts
@@ -8,21 +8,21 @@ export class JenkinsPrevious extends GoToAction {
             return null;
         }
 
-        const selector = JenkinsHelpers.getBreadcrumbItemSelector(bodyElement);
-        const breadCrumbListItems = doc.querySelectorAll(selector);
-        if (breadCrumbListItems) {
-            var urlParts = JenkinsHelpers.splitUrlPath(urlString);
-            for (var index = urlParts.length - 1; index >= 2; index--) {
-                const lastPart = urlParts[index];
-                const isUrlToRun =
-                    "job" === urlParts[index - 2] &&
-                    JenkinsHelpers.isInteger(lastPart);
-                if (isUrlToRun) {
-                    const prevRun = Number.parseInt(lastPart, 10) - 1;
-                    urlParts[index] = prevRun.toString();
-                    const rebuiltPath = urlParts.join("/");
-                    return JenkinsHelpers.buildUrl(urlString, rebuiltPath);
-                }
+        const jenkinsPage = JenkinsHelpers.parsePage(
+            bodyElement,
+            urlString
+        );
+        var urlParts = jenkinsPage.urlParts;
+        for (var index = urlParts.length - 1; index >= 2; index--) {
+            const lastPart = urlParts[index];
+            const isUrlToRun =
+                "job" === urlParts[index - 2] &&
+                JenkinsHelpers.isInteger(lastPart);
+            if (isUrlToRun) {
+                const prevRun = Number.parseInt(lastPart, 10) - 1;
+                urlParts[index] = prevRun.toString();
+                const rebuiltPath = urlParts.join("/");
+                return JenkinsHelpers.buildUrl(urlString, rebuiltPath);
             }
         }
         return null;
