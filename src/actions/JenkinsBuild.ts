f9b44302f30585e041447f35fb5a8169b7e61a83
Olivier Dagenais <oli@bravodelta.ca>
2026-02-04 22:18:06 -0500

    refactor: centralize, re-use code in JenkinsPage

--- src/actions/JenkinsBuild.ts
+++ src/actions/JenkinsBuild.ts
@@ -1,5 +1,6 @@
 import { JenkinsHelpers } from "../JenkinsHelpers";
 import { Action } from "../Action";
+import { Crumb } from "../Crumb";
 
 export interface Executable {
     number?: number;
@@ -79,46 +80,35 @@ export class JenkinsBuild implements Action {
 
         /* <a> elements with an href ending in "/build?delay=0sec" */
         const linkSelector = "a[href$='/build?delay=0sec']";
-        const crumbSelector =
-            JenkinsHelpers.getBreadcrumbItemSelector(bodyElement);
-        const crumbListItems = doc.querySelectorAll(crumbSelector);
+        const jenkinsPage = JenkinsHelpers.parsePage(
+            bodyElement,
+            url
+        );
         if (bodyElement.querySelector(linkSelector)) {
-            const index = crumbListItems.length - 1;
-            const listItem = crumbListItems.item(index);
-            const anchor = listItem.querySelector("a");
-            if (anchor) {
-                const path = anchor.getAttribute("href");
-                if (path) {
+            const crumb = jenkinsPage.lastCrumb();
+            const destinationUrl = jenkinsPage.buildUrl(
+                crumb.href + "build",
+                "delay=0sec"
+            );
+            return new Request(destinationUrl, PostOptions);
+        }
+        else {
+            let result : Request | null = null;
+            jenkinsPage.reverseCrumb((crumb: Crumb, key?: number) => {
+                const hrefParts = JenkinsHelpers.splitPath(crumb.href);
+                if ("job" == hrefParts[hrefParts.length - 2]) {
                     const destinationUrl = JenkinsHelpers.buildUrl(
                         url,
-                        path + "build",
+                        crumb.href + "build",
                         "delay=0sec"
                     );
-                    return new Request(destinationUrl, PostOptions);
+                    result = new Request(destinationUrl, PostOptions);
+                    return false;
                 }
-            }
-        } else {
-            for (var index = crumbListItems.length - 1; index > 1; index--) {
-                const item = crumbListItems.item(index);
-                const anchor = item.querySelector("a");
-                if (anchor) {
-                    const href = anchor.getAttribute("href");
-                    if (href) {
-                        const hrefParts = JenkinsHelpers.splitPath(href);
-                        if ("job" == hrefParts[hrefParts.length - 2]) {
-                            const destinationUrl = JenkinsHelpers.buildUrl(
-                                url,
-                                href + "build",
-                                "delay=0sec"
-                            );
-                            return new Request(destinationUrl, PostOptions);
-                        }
-                    }
-                }
-            }
+                return true;
+            });
+            return result;
         }
-
-        return null;
     }
 
     async queueRun(request: Request) {
