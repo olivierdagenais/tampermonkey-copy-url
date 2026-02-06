2c83407173d4e27f2826260cb6ba3983ee5ae2fe
Olivier Dagenais <oli@bravodelta.ca>
2026-02-05 20:38:47 -0500

    refactor: simplify with JenkinsPage

--- src/parsers/Jenkins.ts
+++ src/parsers/Jenkins.ts
@@ -1,6 +1,7 @@
 import { AbstractParser } from "./AbstractParser";
 import { JenkinsHelpers } from "../JenkinsHelpers";
 import { Link } from "../Link";
+import { Crumb } from "../Crumb";
 
 // Right-Pointing Double Angle Quotation Mark
 const chevron = "\u00BB";
@@ -12,45 +13,41 @@ export class Jenkins extends AbstractParser {
             return null;
         }
 
-        const selector = JenkinsHelpers.getBreadcrumbItemSelector(bodyElement);
-        const urlParts = JenkinsHelpers.splitUrlPath(url);
+        const jenkinsPage = JenkinsHelpers.parsePage(
+            bodyElement,
+            url
+        );
+        const urlParts = jenkinsPage.urlParts;
         const isUrlToRunConsole =
             "console" === urlParts[urlParts.length - 1] &&
             JenkinsHelpers.isInteger(urlParts[urlParts.length - 2]);
-        var linkText = ``;
-        const listItems: NodeListOf<HTMLElement> =
-            doc.querySelectorAll(selector);
-        listItems.forEach((value: HTMLElement, key: number) => {
+        let linkText = ``;
+        jenkinsPage.eachCrumb((crumb: Crumb, key: number) => {
             if (key > 0) {
-                const anchor: HTMLAnchorElement | null =
-                    value.querySelector("a");
-                if (anchor) {
-                    var isUrlToRun = false;
-                    const href: string | null = anchor.getAttribute("href");
-                    if (href) {
-                        const hrefParts = JenkinsHelpers.splitPath(href);
-                        const lastPart = hrefParts[hrefParts.length - 1];
-                        if (JenkinsHelpers.isInteger(lastPart)) {
-                            isUrlToRun = !(
-                                "node" === hrefParts[hrefParts.length - 2] &&
-                                "execution" === hrefParts[hrefParts.length - 3]
-                            );
-                        }
+                let isUrlToRun = false;
+                const href = crumb.href;
+                const hrefParts = JenkinsHelpers.splitPath(href);
+                const lastPart = hrefParts[hrefParts.length - 1];
+                if (JenkinsHelpers.isInteger(lastPart)) {
+                    isUrlToRun = !(
+                        "node" === hrefParts[hrefParts.length - 2] &&
+                        "execution" === hrefParts[hrefParts.length - 3]
+                    );
+                }
+                if (key > 1) {
+                    if (isUrlToRun) {
+                        linkText += ` `;
                     }
-                    if (key > 1) {
-                        if (isUrlToRun) {
-                            linkText += ` `;
-                        }
-                        else {
-                            linkText += ` ${chevron} `;
-                        }
-                    }
-                    linkText += anchor.text.trim();
-                    if (key == listItems.length - 1 && isUrlToRunConsole) {
-                        linkText += ` ${chevron} Console Output`;
+                    else {
+                        linkText += ` ${chevron} `;
                     }
                 }
+                linkText += crumb.name?.trim();
+                if (key == jenkinsPage.crumbs.length - 1 && isUrlToRunConsole) {
+                    linkText += ` ${chevron} Console Output`;
+                }
             }
+            return true;
         });
 
         const result: Link = {
