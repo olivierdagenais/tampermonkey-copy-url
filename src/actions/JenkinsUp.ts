2f22013b84e141c56cf2b76be19881d1daf8921a
Olivier Dagenais <oli@bravodelta.ca>
2026-02-05 21:55:15 -0500

    fix: JenkinsUp uses JenkinsPage, exposes bad tests

--- src/actions/JenkinsUp.ts
+++ src/actions/JenkinsUp.ts
@@ -8,11 +8,14 @@ export class JenkinsUp extends GoToAction {
             return null;
         }
 
-        const selector = JenkinsHelpers.getBreadcrumbItemSelector(bodyElement);
-        const breadCrumbListItems = doc.querySelectorAll(selector);
-        if (breadCrumbListItems && breadCrumbListItems.length > 1) {
-            var offset = 2;
-            const urlParts = JenkinsHelpers.splitUrlPath(urlString);
+        const jenkinsPage = JenkinsHelpers.parsePage(
+            bodyElement,
+            urlString
+        );
+        const numCrumbs = jenkinsPage.crumbs.length;
+        if (numCrumbs > 1) {
+            let offset = 2;
+            const urlParts = jenkinsPage.urlParts;
             const lastPart = urlParts[urlParts.length - 1];
             // execution/node/5/
             if (
@@ -21,53 +24,16 @@ export class JenkinsUp extends GoToAction {
                 "node" == urlParts[urlParts.length - 2] &&
                 "execution" == urlParts[urlParts.length - 3]
             ) {
-                const penultimateItem = breadCrumbListItems.item(
-                    breadCrumbListItems.length - 2
-                );
-                const anchor = penultimateItem.querySelector("a");
-                if (anchor) {
-                    const path = anchor.getAttribute("href");
-                    if (path) {
-                        return JenkinsHelpers.buildUrl(
-                            urlString,
-                            path + "flowGraphTable/"
-                        );
-                    }
-                }
-                return null;
+                const penultimateCrumb = jenkinsPage.penultimateCrumb();
+                const path = penultimateCrumb.href;
+                return jenkinsPage.buildUrl(path + "flowGraphTable/");
             }
-            const specialCases = new Set<string>(
-                [
-                    "configure",
-                    "consoleFull",
-                    "console",
-                ]);
-            if (specialCases.has(lastPart)) {
-                offset = 1;
-            }
-            for (let i = breadCrumbListItems.length - offset; i >= 0; i--) {
-                const item = breadCrumbListItems.item(i);
-                const anchor = item.querySelector("a");
-                if (anchor) {
-                    const path = anchor.getAttribute("href");
-                    if (path) {
-                        return JenkinsHelpers.buildUrl(urlString, path);
-                    }
-                }
-            }
+            const crumb = jenkinsPage.crumbs[numCrumbs - offset];
+            const path = crumb.href;
+            return jenkinsPage.buildUrl(path);
         }
-
-        if (breadCrumbListItems) {
-            const firstItem = breadCrumbListItems.item(0);
-            const anchor = firstItem.querySelector("a");
-            if (anchor) {
-                const path = anchor.getAttribute("href");
-                if (path) {
-                    return JenkinsHelpers.buildUrl(urlString, path);
-                }
-            }
-        }
-
-        return null;
+        const firstCrumb = jenkinsPage.firstCrumb();
+        const path = firstCrumb.href;
+        return jenkinsPage.buildUrl(path);
     }
 }
