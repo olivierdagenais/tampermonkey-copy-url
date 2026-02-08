fbe412588666b945a06974a368f6da89c3001fd9
Olivier Dagenais <oli@bravodelta.ca>
2026-02-07 20:30:37 -0500

    refactor: de-duplicate w/pageOrRootAction() method

--- src/actions/JenkinsPipelineSyntax.ts
+++ src/actions/JenkinsPipelineSyntax.ts
@@ -8,31 +8,13 @@ export class JenkinsPipelineSyntax extends GoToAction {
             return null;
         }
 
-        /* <a> elements with an href ending in "pipeline-syntax" */
-        const selector = "a[href$='/pipeline-syntax']";
-        const crumbSelector =
-            JenkinsHelpers.getBreadcrumbItemSelector(bodyElement);
-        const crumbListItems = doc.querySelectorAll(crumbSelector);
-        var index = -1;
-        if (1 == crumbListItems.length) {
-            index = 0;
-        } else if (bodyElement.querySelector(selector)) {
-            index = crumbListItems.length - 1;
-        }
-        if (index >= 0) {
-            const listItem = crumbListItems.item(index);
-            const anchor = listItem.querySelector("a");
-            if (anchor) {
-                const path = anchor.getAttribute("href");
-                if (path) {
-                    return JenkinsHelpers.buildUrl(
-                        url,
-                        path + "pipeline-syntax"
-                    );
-                }
-            }
-        }
-
-        return null;
+        const jenkinsPage = JenkinsHelpers.parsePage(
+            bodyElement,
+            url
+        );
+        const action = "pipeline-syntax";
+        return jenkinsPage.pageOrRootAction(
+            action,
+        );
     }
 }
